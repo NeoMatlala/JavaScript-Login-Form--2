634cd3d6eb52c0d5ecd123e2c907dfbc1c9aaad9
NeoMatlala <neomatlala70@gmail.com>
2021-01-03 20:00:03 +0200

    mobile layout complete

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
+const signUpLink = document.getElementById('dont-have-account');
+const SignInLink = document.getElementById('already-have-account');
+const loginIn = document.getElementById('login-in');
+const loginUp = document.getElementById('login-up');
+
 
 /* ----- EVENT LISTENERS ----- */
 
@@ -15,3 +20,26 @@ signUpButton.addEventListener('click', () =>
 signInButton.addEventListener('click', () => 
     container.classList.remove('right-panel-active')
 )
+
+
+
+/* ----- SHOW AND HIDE LOGINS (mobile view) ------ */
+signUpLink.addEventListener('click', () => {
+    // Remove classes first if they exist
+    loginIn.classList.remove('block')
+    loginUp.classList.remove('none')
+
+    // Add classes
+    loginIn.classList.add('none')
+    loginUp.classList.add('block')
+})
+
+SignInLink.addEventListener('click', () => {
+    // Remove classes first if they exist
+    loginIn.classList.remove('none')
+    loginUp.classList.remove('block')
+
+    // Add classes
+    loginIn.classList.add('block')
+    loginUp.classList.add('none')
+})
